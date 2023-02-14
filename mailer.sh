7d4af311ded76b5563cf03a5f77feda995b9d903
AhmedSherif-rf <ahmed.exeroot@gmail.com>
2023-02-14 05:53:43 -0500

    remove some comments

--- mailer.sh
+++ mailer.sh
@@ -1,16 +1,4 @@
 #!/bin/bash 
-#cat users.txt | tr '.' ' ' | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | sed 's/ /./g' > ids.txt
-#while read -r a; do
-#  if [ -z "$a" ]
-#    then
-#      echo "There is No ID specified."
-#  elif [ $((a%2))  -eq 1 ]
-#    then 	  
-#      echo "ID is Odd Number."
-#  else 
-#      echo "ID is Even Number."
-#  fi	
-#done < ids.txt
 cat users.txt | grep @ > tmp
 cat tmp | tr '.' ' ' | sed -e 's/[^0-9]/ /g' -e 's/^ *//g' -e 's/ *$//g' | tr -s ' ' | sed 's/ /./g' > ids.txt
 #awk -F' ' '{ print $2 }' tmp-mail.txt > tmp
@@ -21,3 +9,4 @@ while read -r a b c; do
     echo "The ID $c of the Email $b is Even.."
   fi
 done < tmp
+rm -rf tmp ids.txt
